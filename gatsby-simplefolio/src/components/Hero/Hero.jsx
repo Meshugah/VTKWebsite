237ae42374910cb7dcf030df70e0dce3a88541f5
vigneshkarthikeyan <vignesh@alumni.purdue.edu>
2020-08-03 13:32:56 +0530

    Landing Page, no Logo

---
+++ gatsby-simplefolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import PortfolioContext from '../../context/context';
+
+const Header = () => {
+  const { hero } = useContext(PortfolioContext);
+  const { title, cta } = hero;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="hero" className="jumbotron">
+      <Container>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <h1 className="hero-title">
+            {title || 'Hi, the '}
+            <span className="text-color-main">VTK Industries </span>Website is being redesigned{' '}
+            <br />
+          </h1>
+        </Fade>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+          <p className="hero-cta">
+            <span className="cta-btn cta-btn--hero">
+              <a href="mailto: vtk@vtkindustries.com">{cta || 'Contact us'}</a>
+            </span>
+          </p>
+        </Fade>
+      </Container>
+    </section>
+  );
+};
+
+export default Header;
